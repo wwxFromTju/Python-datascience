670dbb60239d88dbfceb43122b1350af9125ec64
wwxfromtju <wxwang@tju.edu.cn>
2016-05-26 19:52:46 +0800

    end DataFrame

--- python_data_analysis/python_data_analysis/ch05/ch05-2.py
+++ python_data_analysis/python_data_analysis/ch05/ch05-2.py
@@ -43,8 +43,40 @@ frame_columns_without['debt'] = np.arange(5.)
 # 如果赋值的是Series,那么会精确匹配DataFrame的索引,同时空位被填上缺失值
 val = Series([-1.2, -1.5, -1.7], index=['two', 'four', 'five'])
 frame_columns_without['debt'] = val
-print frame_columns_without
+# print frame_columns_without
 
 # 为不存在的值赋值会创建出一个新列
 frame_columns_without['estern'] = frame_columns_without.state == 'Ohio'
 # print frame_columns_without
+
+# del用于删除列
+del frame_columns_without['estern']
+# print frame_columns_without
+# print frame_columns_without.columns
+
+# 对于嵌套的dict,将解释为:外层的键为列的名字,内层的键为行的索引
+# 对于没有对应值的,补NaN
+pop = {'Nevada': {2001: 2.4, 2002: 2.9},
+       'Ohio': {2000: 1.5, 2001: 1.7, 2002: 3.6}}
+frame_in_dict = DataFrame(pop)
+# print frame_in_dict
+
+# 转置DataFrame,将行列对调
+# print frame_in_dict.T
+
+# 可以显示地指定索引,这样可以忽略掉一些行索引,也可增加一些全部为NaN的行
+frame_obvious = DataFrame(pop, index=[2001, 2002, 2003])
+# print frame_obvious
+
+# 对于正常的字典, 则键为列名,值为对应行
+pdata = {'Ohio': frame_in_dict['Ohio'][:-1],
+         'Nevada': frame_in_dict['Nevada'][:2]}
+# print DataFrame(pdata)
+
+# 可以设置DataFrame对应的columns名与index名
+frame_in_dict.index.name = 'year'
+frame_in_dict.columns.name = 'state'
+# print frame_in_dict
+
+# 可以以2维ndarray的形式返回DataFrame中的数据
+# print frame_in_dict.values
