ad7b6155abb6b0ad2e172b7115279130e8071814
wwxfromtju <wxwang@tju.edu.cn>
2016-05-29 19:44:12 +0800

    ok

--- python_data_analysis/python_data_analysis/ch05/ch05-10.py
+++ python_data_analysis/python_data_analysis/ch05/ch05-10.py
@@ -4,6 +4,7 @@
 import pandas as pd
 import numpy as np
 import pandas.io.data as web
+# from pandas.datareader import web
 from pandas import Series, DataFrame
 
 # 计算来自Yahoo!Finance的股票价格和成交量
@@ -15,4 +16,20 @@ for ticker in ['AAPL', 'IBM', 'MSFT', 'GOOG']:
 price = DataFrame({tic: data['Adj Close'] for tic, data in all_data.iteritems()})
 volume = DataFrame({tic: data['Volume'] for tic, data in all_data.iteritems()})
 
-# 计算价格白枫树
+# 计算价格百分数变化
+returns = price.pct_change()
+# print returns.tail()
+
+# series的corr方法用于计算两个series中重叠的,非NA的,按索引对齐的值的相关系数
+# print returns.MSFT.corr(returns.IBM)
+# cov 用与计算协方差
+# print returns.MSFT.cov(returns.IBM)
+
+# 对DataFrame就是直接统计各列间的如:相关系数,协方差等等
+# print returns.corr()
+# print returns.cov()
+
+# 利用DataFrame的corrwith可以计算列或者行和另外一个Series或者DataFrame之间的相关系数
+# print returns.corrwith(returns.IBM)
+# 传入为DataFrame则会按照列名配对
+# print returns.corrwith(volume)
