8e22ad5285022258f96929f5105240621d3401b4
wwxfromtju <wxwang@tju.edu.cn>
2016-05-31 20:15:52 +0800

    change the annotate

--- python_data_analysis/python_data_analysis/ch02/ch02-1.py
+++ python_data_analysis/python_data_analysis/ch02/ch02-1.py
@@ -32,44 +32,44 @@ def top_counts(count_dict, n = 10):
 path = '../../pydata-book-master/ch02/usagov_bitly_data2012-03-16-1331923249.txt'
 
 records = [json.loads(line) for line in open(path)]
-# print records
+print records
 
 time_zone = [rec['tz'] for rec in records if 'tz' in rec]
-# print time_zone
+print time_zone
 
 counts = get_counts(time_zone)
 counts2 = get_counts2(time_zone)
-# print counts,'\n',counts2
+print counts,'\n',counts2
 
 top_counts_10 = top_counts(counts)
-# print top_counts_10
+print top_counts_10
 
 frame = DataFrame(records)
 tz_counts = frame['tz'].value_counts()
-# print tz_counts[:10]
+print tz_counts[:10]
 
 clean_tz = frame['tz'].fillna("Missing")
 clean_tz[clean_tz == ''] = 'Unknown'
 tz_counts_clean = clean_tz.value_counts()
-# print tz_counts_clean[:10]
+print tz_counts_clean[:10]
 
 # 在虚拟环境下,matplotlib不能显示出来,推荐采用在命令行中直接 ipython --pylib然后运行本文件代码
 # tz_counts_clean[:10].plot(kind='barh', rot=0)
 
 result = Series([x.split()[0] for x in frame.a.dropna()])
-# print result[:5]
-# print  result.value_counts()[:8]
+print result[:5]
+print  result.value_counts()[:8]
 
 cframe = frame[frame.a.notnull()]
 operating_system = np.where(cframe['a'].str.contains('Windows'), 'Windows', 'Not Windows')
-# print operating_system[:5]
+print operating_system[:5]
 
 by_tz_os = cframe.groupby(['tz', operating_system])
 agg_counts = by_tz_os.size().unstack().fillna(0)
-# print agg_counts[:10]
+print agg_counts[:10]
 
 indexer = agg_counts.sum(1).argsort()
-# print indexer[:10]
+print indexer[:10]
 
 count_subset = agg_counts.take(indexer)[-10:]
 print count_subset
--- python_data_analysis/python_data_analysis/ch02/ch02-3.py
+++ python_data_analysis/python_data_analysis/ch02/ch02-3.py
@@ -21,8 +21,8 @@ def get_quantile_count(group, q=0.5):
 
 columns = ['name', 'sex', 'births']
 names1880 = pd.read_csv('../../pydata-book-master/ch02/names/yob1880.txt', names=columns)
-# print names1880
-# print names1880.groupby('sex').births.sum()
+print names1880
+print names1880.groupby('sex').births.sum()
 
 years = range(1880, 2011)
 pieces = []
@@ -34,12 +34,12 @@ for year in years:
     frame['year'] = year
     pieces.append(frame)
 
-# print pieces
+print pieces
 names = pd.concat(pieces, ignore_index=True)
-# print names
+print names
 
 total_births = names.pivot_table('births', index='year', columns='sex', aggfunc=sum)
-# print total_births.tail()
+print total_births.tail()
 
 # 注意一下,下面这个语句需要在ipython --pylab模式下运行
 # total_births.plot(title='Total births by sex and year')
@@ -47,26 +47,26 @@ total_births = names.pivot_table('births', index='year', columns='sex', aggfunc=
 keys = ['year', 'sex']
 
 names = names.groupby(keys).apply(add_prop)
-# print names
+print names
 
 np.allclose(names.groupby(keys).prop.sum(), 1)
 
 top1k = names.groupby(keys).apply(get_top1k)
-# print top1k
+print top1k
 
 pieces2 = []
 for year, group in names.groupby(['year', 'sex']):
     pieces2.append(group.sort_index(by='births', ascending=False)[:1000])
 top1000 = pd.concat(pieces2, ignore_index=True)
-# print top1000
+print top1000
 
 boys = top1k[top1k.sex == 'M']
 girls = top1k[top1k.sex == 'F']
-# print boys
-# print girls
+print boys
+print girls
 
 total_births2 = top1k.pivot_table('births', index='year', columns='name', aggfunc=sum)
-# print total_births2
+print total_births2
 
 subset = total_births2[['John', 'Harry', 'Mary', 'Marilyn']]
 
@@ -78,19 +78,19 @@ table = top1k.pivot_table('prop', index='year', columns='sex', aggfunc=sum)
 # table.plot(title='Sum of table1k.prop by year and sex', yticks=np.linspace(0, 1.2, 13), xticks=range(1880, 2020, 10))
 
 df = boys[boys.year == 2010]
-# print df
+print df
 
 prop_cumsum = df.sort_index(by='prop', ascending=False).prop.cumsum()
-# print prop_cumsum[:10]
-# print prop_cumsum.searchsorted(0.5)
+print prop_cumsum[:10]
+print prop_cumsum.searchsorted(0.5)
 
 df = boys[boys.year == 1900]
 in1900 = df.sort_index(by='prop', ascending=False).prop.cumsum()
-# print in1900.searchsorted(0.5) + 1
+print in1900.searchsorted(0.5) + 1
 
 diversity = top1k.groupby(['year', 'sex']).apply(get_quantile_count)
 diversity = diversity.unstack('sex')
-# print diversity.head()
+print diversity.head()
 
 # 注意下面的代码需要在ipython --pylab模式下运行
 # diversity.plot(title='Number of popular names in top 50%')
@@ -100,13 +100,13 @@ lase_letters = names.name.map(get_last_letter)
 lase_letters.name = 'last_letter'
 
 table = names.pivot_table('births', index=lase_letters, columns=['sex', 'year'], aggfunc=sum)
-# print table
+print table
 subtable = table.reindex(columns=[1910, 1960, 2010], level='year')
-# print subtable.head()
-# print subtable.sum()
+print subtable.head()
+print subtable.sum()
 
 letter_prop = subtable / subtable.sum().astype(float)
-# print letter_prop
+print letter_prop
 
 #下面3行代码需要在ipython --pylab模式下运行
 # fig, axes = plt.subplots(2, 1, figsize=(10, 8))
@@ -115,7 +115,7 @@ letter_prop = subtable / subtable.sum().astype(float)
 
 letter_prop = table / table.sum().astype(float)
 dny_ts = letter_prop.ix[['d', 'n', 'y'], 'M'].T
-# print dny_ts.head()
+print dny_ts.head()
 
 #下面的代码需要在ipython --pylab模式下面运行
 # dny_ts.plot()
@@ -123,10 +123,10 @@ dny_ts = letter_prop.ix[['d', 'n', 'y'], 'M'].T
 all_names = top1k.name.unique()
 mask = np.array(['lesl' in x.lower() for x in all_names])
 lesley_like = all_names[mask]
-# print lesley_like
+print lesley_like
 
 filtered = top1k[top1k.name.isin(lesley_like)]
-# print filtered.groupby('name').births.sum()
+print filtered.groupby('name').births.sum()
 
 table = filtered.pivot_table('births', index='year', columns='sex', aggfunc='sum')
 table = table.div(table.sum(1), axis=0)
