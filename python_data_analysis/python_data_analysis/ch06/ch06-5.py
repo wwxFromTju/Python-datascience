beca77886ccfc55a36300638bd08214b977778d6
wwxfromtju <wxwang@tju.edu.cn>
2016-06-02 20:17:38 +0800

    just ok

--- python_data_analysis/python_data_analysis/ch06/ch06-5.py
+++ python_data_analysis/python_data_analysis/ch06/ch06-5.py
@@ -1,3 +1,32 @@
 #!/usr/bin/env python
 # encoding=utf-8
 
+import json
+
+import pandas as pd
+import numpy as np
+from pandas import Series, DataFrame
+
+# JSON(JavaScript Object Notation)
+
+obj = """
+{"name": "Wes",
+ "places_lived": ["United States", "Spain", "Germany"],
+ "pet": null,
+ "siblings": [{"name": "Scott", "age": 25, "pet": "Zuko"},
+                {"name": "Katie", "age": 33, "pet": "Cisco"}]
+}
+"""
+
+#  读取JSON字符串并转换为Python形式
+result = json.loads(obj)
+print result
+
+# 将Python对象转换为JSON格式
+asjson = json.dumps(result)
+print asjson
+
+# 将JSON对象转换为DataFrame
+# 向Python构造器传入一组JSON对象,并选取数据字段的子集
+siblings = DataFrame(result['siblings'], columns=['name', 'age'])
+print siblings
