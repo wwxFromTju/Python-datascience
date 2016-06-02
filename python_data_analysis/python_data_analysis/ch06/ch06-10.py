a780188ff0cbcf89dfea528bb676da38e43299f8
wwxfromtju <wxwang@tju.edu.cn>
2016-06-02 21:51:32 +0800

    Erro in API

--- python_data_analysis/python_data_analysis/ch06/ch06-10.py
+++ python_data_analysis/python_data_analysis/ch06/ch06-10.py
@@ -2,11 +2,21 @@
 # encoding=utf-8
 
 import requests
+import json
 
 import pandas as pd
 import numpy as np
 from pandas import Series, DataFrame
 
+# 发送一个HTTP GET请求
+# 旧的API,现在无法运行了
 url = 'http://search.twitter.com/search.json?q=python%20pandas'
 resp = requests.get(url)
 print resp
+# 很多Web API返回的是JSON字符串
+data = json.loads(resp.text)
+print data.keys()
+tweet_fields = ['created_at', 'from_usrt', 'id', 'text']
+tweets = DataFrame(data['results'], columns=tweet_fields)
+print tweets
+print tweets.ix[7]
