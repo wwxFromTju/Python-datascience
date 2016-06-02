00059576be260dbb2bd3c4bf62c256f0e173a40d
wwxfromtju <wxwang@tju.edu.cn>
2016-06-02 22:01:53 +0800

    ok

--- python_data_analysis/python_data_analysis/ch06/ch06-11.py
+++ python_data_analysis/python_data_analysis/ch06/ch06-11.py
@@ -4,5 +4,34 @@
 import pandas as pd
 import numpy as np
 from pandas import Series, DataFrame
+import pandas.io.sql as sql
 
 # 使用数据库
+import sqlite3
+
+query = """
+CREATE TABLE test
+(a VARCHAR(20), b VARCHAR(20),
+c REAL,         d INTEGER
+);
+"""
+
+con = sqlite3.connect(':memory:')
+con.execute(query)
+con.commit()
+
+data = [('Atlanta', 'Georgia', 1.25, 6),
+        ('Tallahassee', 'Florida', 2.6, 3),
+        ('Sacramento', 'California', 1.7, 5)]
+stmt = "INSERT INTO test VALUES(?, ?, ?, ?)"
+
+con.executemany(stmt, data)
+con.commit()
+
+curosr = con.execute('select * from test')
+rows = curosr.fetchall()
+print rows
+print curosr.description
+print DataFrame(rows, columns=zip(*curosr.description)[0])
+
+print sql.read_sql('select * from test', con)
