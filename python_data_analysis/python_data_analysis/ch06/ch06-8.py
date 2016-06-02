19902370c749a3d4123ed285fa09b6ddc68736ae
wwxfromtju <wxwang@tju.edu.cn>
2016-06-02 21:05:01 +0800

    yo

---
+++ python_data_analysis/python_data_analysis/ch06/ch06-8.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+import pandas as pd
+import numpy as np
+from pandas import Series, DataFrame
+
+# 二进制文件
+
+# 正常读取文本文件
+frame = pd.read_csv('ex1.csv')
+print frame
+#保存为2jing zhi
+#
