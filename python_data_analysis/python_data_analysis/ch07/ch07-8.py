7e9d32ada0e23438aebecaa220644b5510f84a2e
wwxfromtju <wxwang@tju.edu.cn>
2016-06-05 09:58:09 +0800

    ok

--- python_data_analysis/python_data_analysis/ch07/ch07-8.py
+++ python_data_analysis/python_data_analysis/ch07/ch07-8.py
@@ -1,1 +1,19 @@
-#
+#!/usr/bin/env python
+# encoding=utf-8
+
+import pandas as pd
+import numpy as np
+from pandas import Series, DataFrame
+
+# 替换值
+# 类似于fillna, 对值进行替换
+data = Series([1., -999., 2., -999., -1000., 3.])
+print data
+# 我们可以约定-999是缺失值,但是NaN没有二义性
+print data.replace(-999, np.nan)
+# 一次替换多个
+print data.replace([-999, -1000], np.nan)
+# 不同值替换成不同的值,注意位置对应
+print data.replace([-999, -1000], [np.nan, 0])
+# 字典: key是要被替换的, value是替换的
+print data.replace({-999: np.nan, -1000: 0})
