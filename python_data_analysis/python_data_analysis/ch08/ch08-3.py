4efabc4b6043fdd90a2c566a2fcdbfae76b381bd
wwxfromtju <wxwang@tju.edu.cn>
2016-06-08 22:46:50 +0800

    enjoy the code

--- python_data_analysis/python_data_analysis/ch08/ch08-3.py
+++ python_data_analysis/python_data_analysis/ch08/ch08-3.py
@@ -2,6 +2,7 @@
 # encoding=utf-8
 
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from pandas import Series, DataFrame
 from datetime import datetime
---
+++ python_data_analysis/python_data_analysis/ch08/ch08-4.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pandas import Series, DataFrame
+
+# 常见图形
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+
+rect = plt.Rectangle((0.2, 0.75), 0.4, 0.15, color='k', alpha=0.3)
+circ = plt.Circle((0.7, 0.2), 0.15, color='r', alpha=0.3)
+pgon = plt.Polygon([[0.15, 0.15], [0.35, 0.4], [0.2, 0.6]], color='g', alpha=0.5)
+
+ax.add_patch(rect)
+ax.add_patch(circ)
+ax.add_patch(pgon)
