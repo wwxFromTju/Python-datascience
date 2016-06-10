ca42489dd1a701db60f59f9a9d14dc9636e35d7a
wwxfromtju <wxwang@tju.edu.cn>
2016-06-10 19:51:52 +0800

    ok

---
+++ python_data_analysis/python_data_analysis/ch09/ch09-2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+import pandas as pd
+import numpy as np
+from pandas import Series, DataFrame
+
+people = DataFrame(np.random.randn(5, 5),
+                   columns=['a', 'b', 'c', 'd', 'e'],
+                   index=['Joe', 'Steve', 'Wes', 'Jim', 'Travis'])
+print people
+people.ix[2:3, ['b', 'c']] = np.nan
+print people
+
+mapping = {'a': 'red', 'b': 'red', 'c': 'blue',
+           'd': 'blue', 'e': 'red', 'f': 'orange'}
+# 使用字典来转换相应分组
+by_column = people.groupby(mapping, axis=1)
+print by_column.sum()
+
+# 对于Series可以看成固定大小的索引
+map_series = Series(mapping)
+print map_series
+print people.groupby(map_series, axis=1).count()
