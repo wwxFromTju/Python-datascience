11a2743add7def8cac330b4ac1f6c0ea689e7c2a
wwxfromtju <wxwang@tju.edu.cn>
2016-06-23 11:52:43 +0800

    10-15 end

--- python_data_analysis/python_data_analysis/ch10/ch10-15.py
+++ python_data_analysis/python_data_analysis/ch10/ch10-15.py
@@ -10,6 +10,16 @@ from pandas import Series, DataFrame
 
 # 固定频率的数据集,通常会将时间信息分开存放在多个列中。
 # 比如宏观经济数据集中, 年度和季度就分开存放在不同的列中
+data = pd.read_csv('macrodata.csv')
+print data.year
+print data.quarter
+
+# 将这两个数组以及一个频率传入PeriodIndex,可以合并成DataFrame的一个索引
+index = pd.PeriodIndex(year=data.year, quarter=data.quarter, freq='Q-DEC')
+print index
+
+data.index = index
+print data.infl5
 
 
 
