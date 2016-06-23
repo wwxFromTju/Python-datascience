e44865c50133c9a4dea806a7e21716e4be1f853d
wwxfromtju <wxwang@tju.edu.cn>
2016-06-23 11:44:38 +0800

    10-15 start

---
+++ python_data_analysis/python_data_analysis/ch10/ch10-15.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+import pandas as pd
+import numpy as np
+import pytz
+from pandas import Series, DataFrame
+
+# 通过数组创建PeriodIndex
+
+# 固定频率的数据集,通常会将时间信息分开存放在多个列中。
+# 比如宏观经济数据集中, 年度和季度就分开存放在不同的列中
+
+
+
+
+
