1b62120072ec214fec3cece33956689d81dfc036
wwxfromtju <wxwang@tju.edu.cn>
2016-06-23 15:10:49 +0800

    10-17 end

--- python_data_analysis/python_data_analysis/ch10/ch10-17.py
+++ python_data_analysis/python_data_analysis/ch10/ch10-17.py
@@ -16,9 +16,15 @@ ts = Series(np.arange(12), index=rng)
 print ts
 
 # 聚合到5分钟中
-print ts.resample()
+print ts.resample('5min').sum()
+# 设置对应的面元边界, 就是区间的边界
+print ts.resample('5min', closed='left').sum()
+print ts.resample('5min', closed='right').sum()
 
+# 最终的时间序列是以各面元右边界的时间戳进行表示的,可以通过label来设置
+print ts.resample('5min', closed='left', label='left').sum()
 
-
-
-
+# 通过loffset设置偏移
+print ts.resample('5min', loffset='-1s').sum()
+# 等价于shift
+print ts.resample('5min').shift(-1).sum()
